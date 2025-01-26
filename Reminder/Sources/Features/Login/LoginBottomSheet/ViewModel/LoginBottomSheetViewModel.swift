b3adafcf7dbf79dfc43afcb2abb1b460cca34e46
Egito Filho <egitofilho4@gmail.com>
2025-01-26 12:29:48 -0300

    error alert

--- Reminder/Sources/Features/Login/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
+++ Reminder/Sources/Features/Login/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
@@ -3,11 +3,12 @@ import Firebase
 
 class LoginBottomSheetViewModel {
     var successResult: ((String) -> Void)?
+    var errorResult: ((String) -> Void)?
     
     func doAuth(userNameLogin: String, password: String) {
         Auth.auth().signIn(withEmail: userNameLogin, password: password) { [weak self] authResult, error in
             if let error = error {
-                print("autenticação não foi feita com sucesso \(error)")
+                self?.errorResult?("Error ao realizar login, verifique as credenciais digitadas")
             } else {
                 self?.successResult?(userNameLogin)
             }
