cab9148de5a459e73967f5b886a5946ee1f78913
Egito Filho <egitofilho4@gmail.com>
2025-02-09 15:10:14 -0300

    create component

--- Reminder/Sources/Constants/Metrics/Metrics-Constants.swift
+++ Reminder/Sources/Constants/Metrics/Metrics-Constants.swift
@@ -8,6 +8,7 @@ public struct Metrics {
     static let huge: CGFloat = 48
     static let enormous: CGFloat = 32
     static let medium: CGFloat = 24
+    static let medier: CGFloat = 16
     static let small: CGFloat = 12
     static let tiny: CGFloat = 8
     static let little: CGFloat = 4
